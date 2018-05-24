bf44e4338bf1a24e15e0dc700e56a7381a786377
Meher Gundavarum <meher@Mehers-MacBook-Pro.local>
2018-05-24 00:23:24 -0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,100 @@
+/*
+ * Create a list that holds all of your cards
+ */
+
+const cards = ['fa-diamond', 'fa-paper-plane-o', 'fa-anchor', 'fa-bolt',
+              'fa-cube', 'fa-leaf', 'fa-bicycle', 'fa-bomb',
+              'fa-diamond', 'fa-paper-plane-o', 'fa-anchor', 'fa-bolt',
+              'fa-cube', 'fa-leaf', 'fa-bicycle', 'fa-bomb'];
+
+// deck selector
+const deck = document.querySelector('.deck');
+
+/*
+ * Display the cards on the page
+ *   - shuffle the list of cards using the provided "shuffle" method below
+ *   - loop through each card and create its HTML
+ *   - add each card's HTML to the page
+ */
+
+const restart = document.querySelector('.restart');
+
+//start a new game on screen load
+window.onload = function() {
+  restart_game();
+};
+
+//restart the game when restart button is clicked
+restart.addEventListener('click', function(){
+  restart_game();
+});
+
+// function to reshuffle or restart the game
+function restart_game() {
+  const shuffledCards = shuffle(cards);
+  const newCard = document.createElement('li');
+
+  deck.innerHTML = "";
+
+  for (let i = 0; i<shuffledCards.length; i++) {
+    const newCard = document.createElement('li');
+    newCard.className = 'card';
+    const newIcon = document.createElement('i');
+    newIcon.classList.add('fa', shuffledCards[i]);
+    newCard.appendChild(newIcon);
+    deck.appendChild(newCard);
+  }
+}
+
+// Shuffle function from http://stackoverflow.com/a/2450976
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
+
+// track Moves
+let moves = document.querySelector('.moves');
+function movesCounter() {
+  moves.innerHTML = '6';
+}
+
+
+
+deck.addEventListener('click', function(evt) {
+  cardOpen(evt);
+
+});
+
+// function for opening the card
+function cardOpen(evt) {
+  evt.target.classList.add('open', 'show');
+}
+// function for closing the card
+function cardClose(evt) {
+  evt.target.classList.remove('open', 'show');
+}
+// function for card match
+function cardMatch(evt) {
+  cardClose(evt);
+  evt.target.classList.add('match');
+}
+/*
+ * set up the event listener for a card. If a card is clicked:
+ *  - display the card's symbol (put this functionality in another function that you call from this one)
+ *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+ *  - if the list already has another card, check to see if the two cards match
+ *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+ *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+ *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+ *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ */
